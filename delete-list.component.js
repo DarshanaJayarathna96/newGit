160aec0116d8e1e8b2a0a67548522eae158319c7
DarshanaJayarathna96 <76892432+DarshanaJayarathna96@users.noreply.github.com>
2021-02-06 19:38:35 +0530

    Add files via upload

---
+++ delete-list.component.js
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react';
+// import View from './todos-list.component'
+// import axios from 'axios';
+
+// class TodoDelete extends Component{
+
+//     deleteEmp(){
+//         axios.delete('http://localhost:3001/tasks/'+this.props.match.params.id)
+//         .then(res => console.log(res.data));
+
+//         // this.props.history.push('/');
+//         // <meta http-equiv="refresh" content='7' url={View} />
+//     }
+    
+
+//     render(){
+//         return(
+//             <div>
+//                 {this.deleteEmp()}
+//             </div>
+//         );
+//     }
+// }
+// export default TodoDelete;
